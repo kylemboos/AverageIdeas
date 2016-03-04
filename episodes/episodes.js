5826df3ec67e416a408756941a5f5c0aad23f888
Kyle Boos <kyle.boos21@gmail.com>
2016-03-04 08:02:56 -0800

    deleting everything to replace

--- episodes/episodes.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('app');
-
-
-app.controller('EpisodesCtrl', function($scope, $window, $location){
-
-
-});
--- home/home.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('app');
-
-
-app.controller('HomeCtrl', function($scope, $window, $location){
-
-
-});
